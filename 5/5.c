127c8b7ae2bc1ae0a108a7357054ab428e510935
Luginin Denis <remisto.android@gmail.com>
2013-01-25 00:50:18 +0600

    5.4

--- 5/5.c
+++ 5/5.c
@@ -1,20 +1,30 @@
-#include <ucontext.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <signal.h>
+#include <ucontext.h>
+#include <sys/time.h>
 
 int NumberOfContexts = 10;
 int StackSize = 1024;
 
-struct ContextOptions{
+struct context_options{
     int ContextId;
     int ContextStatus;
-    ucontext_t Context;
     int ContextSleepTime;
+	char Stack[STACK_SIZE];
+	char ExitStack[STACK_SIZE];
+	ucontext_t Context;
+	ucontext_t ExitContext;
 };
+
 ucontext_t SchedulerContext;
+context_options MyTreads[NumberOfContexts];
+int CurrentContextId = 0;
+
 
 int main(){
+	int i;
+	for (i = 1; i < NumberOfContexts; i++) {
+		MyTreads[i].ContextStatus = 0;//Free Tread
+	}
 	
 	return 0;
 }
